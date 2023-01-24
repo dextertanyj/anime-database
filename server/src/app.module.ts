7e08f6b2ed15f33d8394cd169752da39d72607c9
Dexter Tan <dev@dextertanyj.com>
2023-01-24 09:13:02 +0800

    chore: install graphql packages

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
-
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { GraphQLModule } from "@nestjs/graphql";
+import { join } from "path";
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [
+		GraphQLModule.forRoot<ApolloDriverConfig>({
+			driver: ApolloDriver,
+			typePaths: [join(__dirname, "..", "..", "*.graphql")],
+			debug: true,
+			playground: true,
+		}),
+	],
 })
 export class AppModule {}
