33ab9ffab80c34d7b175f2f5157d60e2c0bb1d1d
Dexter Tan <dev@dextertanyj.com>
2023-01-27 12:00:24 +0800

    feat: configure graphql module with config service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,15 +1,35 @@
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
 import { GraphQLModule } from "@nestjs/graphql";
+import {
+	ApolloServerPluginLandingPageLocalDefault,
+	ApolloServerPluginLandingPageProductionDefault,
+} from "apollo-server-core";
 import { join } from "path";
+
+import { ConfigService } from "./core/config/config.service";
+import { ConfigServiceModule } from "./core/config/config.service.module";
 
 @Module({
 	imports: [
-		GraphQLModule.forRoot<ApolloDriverConfig>({
+		GraphQLModule.forRootAsync<ApolloDriverConfig>({
+			imports: [ConfigServiceModule],
+			inject: [ConfigService],
 			driver: ApolloDriver,
-			typePaths: [join(__dirname, "..", "..", "*.graphql")],
-			debug: true,
-			playground: true,
+			useFactory: (configService: ConfigService) => ({
+				typePaths: [join(__dirname, "..", "..", "*.graphql")],
+				debug: configService.get("environment") === "development",
+				playground: false,
+				plugins: [
+					configService.get("environment") === "production"
+						? ApolloServerPluginLandingPageProductionDefault({
+								footer: false,
+						  })
+						: ApolloServerPluginLandingPageLocalDefault({
+								footer: false,
+						  }),
+				],
+			}),
 		}),
 	],
 })
