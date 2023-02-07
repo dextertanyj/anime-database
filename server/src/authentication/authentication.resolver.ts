0be854bc6df0f6b43f9d12551b5b71507c0b8067
Dexter Tan <dev@dextertanyj.com>
2023-02-07 09:13:49 +0800

    feat: implement user logout

--- server/src/authentication/authentication.resolver.ts
+++ server/src/authentication/authentication.resolver.ts
@@ -1,17 +1,28 @@
 import { UseGuards } from "@nestjs/common";
 import { Mutation, Query, Resolver } from "@nestjs/graphql";
+import { InjectPinoLogger, PinoLogger } from "nestjs-pino";
 
+import { Request } from "src/common/decorators/request.decorator";
 import { User } from "src/common/decorators/user.decorator";
 import { Role, UserSession } from "src/generated/graphql";
 
 import { LocalGuard } from "./local.guard";
-import { SessionGuard } from "./session.guard";
 
 @Resolver()
 export class AuthenticationResolver {
+	constructor(
+		@InjectPinoLogger(AuthenticationResolver.name)
+		private readonly logger: PinoLogger,
+	) {}
+
 	@Query()
-	@UseGuards(SessionGuard)
-	async session(@User() user: Express.User): Promise<UserSession | null> {
+	async session(
+		@Request() request: Express.Request,
+		@User() user: Express.User,
+	): Promise<UserSession | null> {
+		if (!request.isAuthenticated()) {
+			return null;
+		}
 		return { ...user, role: user.role as Role };
 	}
 
@@ -22,7 +33,12 @@ export class AuthenticationResolver {
 	}
 
 	@Mutation()
-	async deleteSession() {
+	async deleteSession(@Request() request: Express.Request) {
+		request.logout((error) => {
+			if (error) {
+				this.logger.warn(error);
+			}
+		});
 		return true;
 	}
 }
---
+++ server/src/common/decorators/request.decorator.ts
@@ -0,0 +1,6 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { GqlExecutionContext } from "@nestjs/graphql";
+
+export const Request = createParamDecorator((_: unknown, ctx: ExecutionContext) => {
+	return GqlExecutionContext.create(ctx).getContext().req;
+});
