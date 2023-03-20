c692d232a82700de0b81f91980e1cea1cb7cf4a4
Dexter Tan <dev@dextertanyj.com>
2023-03-20 16:40:29 +0800

    feat: add episode page skeleton

--- client/src/pages/episode/EpisodePage.tsx
+++ client/src/pages/episode/EpisodePage.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
+import { Button, Collapse, Heading, HStack, IconButton, Stack } from "@chakra-ui/react";
 import { useNavigate, useParams } from "react-router-dom";
 
+import { BiChevronDown, BiChevronUp } from "react-icons/bi";
 import { episode } from "src/hooks/operations/useEpisode";
+import { useIsMobile } from "src/hooks/useIsMobile";
 
 export const EpisodePage = () => {
   const { episodeId } = useParams();
+  const isMobile = useIsMobile();
   const navigate = useNavigate();
 
+  const [showAlternativeTitles, setShowAlternativeTitles] = useState<boolean>(false);
   const { data } = episode.useGet({ id: episodeId ?? "" });
 
   if (!episodeId) {
@@ -17,5 +23,49 @@ export const EpisodePage = () => {
     return null;
   }
 
-  return <div>EpisodePage</div>;
+  return (
+    <Stack w="full" maxW="950px" spacing={4}>
+      <Stack spacing={0}>
+        <HStack justifyContent="space-between">
+          <HStack alignItems="center">
+            <Heading size="xl">{data.episode.title}</Heading>
+            {data.episode.alternativeTitles.length > 0 && (
+              <IconButton
+                icon={
+                  showAlternativeTitles ? <BiChevronUp size={20} /> : <BiChevronDown size={20} />
+                }
+                colorScheme="gray"
+                variant="ghost"
+                borderRadius="20px"
+                size="sm"
+                aria-label="alternative-titles"
+                onClick={() => setShowAlternativeTitles((v) => !v)}
+              />
+            )}
+          </HStack>
+          {isMobile || (
+            <HStack spacing={2}>
+              <Button
+                colorScheme="green"
+                variant="outline"
+                onClick={() => {
+                  navigate("edit");
+                }}
+              >
+                Edit
+              </Button>
+              <Button colorScheme="red" variant="outline">
+                Delete
+              </Button>
+            </HStack>
+          )}
+        </HStack>
+        <Collapse in={showAlternativeTitles}>
+          {data.episode.alternativeTitles.map((title, index) => (
+            <div key={index}>{title}</div>
+          ))}
+        </Collapse>
+      </Stack>
+    </Stack>
+  );
 };
