e55887503c1eb721b3520d506f11fe4fed1ba33e
Dexter Tan <dev@dextertanyj.com>
2023-03-21 18:01:54 +0800

    feat: implement long scalar type parser

--- server/src/core/core.module.ts
+++ server/src/core/core.module.ts
@@ -2,6 +2,7 @@ import { Global, Module } from "@nestjs/common";
 import { PinoLogger } from "nestjs-pino";
 
 import { LoggingPlugin } from "src/common/plugins/logging.plugin";
+import { LongScalar } from "src/common/scalars/long.scalar";
 
 import { ConfigServiceModule } from "./config/config.service.module";
 import { PrismaServiceModule } from "./prisma/prisma.service.module";
@@ -10,7 +11,7 @@ import { RedisServiceModule } from "./redis/redis.service.module";
 @Global()
 @Module({
   imports: [ConfigServiceModule, RedisServiceModule, PrismaServiceModule],
-  providers: [PinoLogger, LoggingPlugin],
+  providers: [PinoLogger, LoggingPlugin, LongScalar],
   exports: [ConfigServiceModule, RedisServiceModule, PrismaServiceModule],
 })
 export class CoreModule {}
